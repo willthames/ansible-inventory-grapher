ce38f81b55d02fbbc2436fef41eb9f1ba98434cf
Will Thames <will@thames.id.au>
2014-07-12 11:34:05 +1000

    Mostly fixed variable graphing
    Graphs now contain more accurate variables (overridden variables
    are removed from parents, inherited variables are removed from
    children)
    
    Made it more testable by modularizing the graphing functions
    Added tests to validate that variable inheritance is displayed
    correctly.

---
+++ lib/grapher/__init__.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+# (c) 2014, Will Thames <will@thames.id.au>
+#
+# ansible-inventory-grapher is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ansible-inventory-grapher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ansible-inventory-grapher.  If not, see <http://www.gnu.org/licenses/>.
+
+import ansible.utils
+import ansible.inventory
+import ansible.constants as C
+
+
+# Cache for parent graph lookups
+_parents = dict()
+
+# Cache for variables
+_vars = dict()
+
+
+class Edge:
+    def __init__(self, source, target):
+        self.source = source
+        self.target = target
+
+    def __repr__(self):
+        return "{} -> {}".format(self.source, self.target)
+
+    def __eq__(self, other):
+        return self.source == other.source and self.target == other.target
+
+    def __hash__(self):
+        return hash(self.source + self.target)
+
+
+class Node:
+    def __init__(self, name, vars={}, leaf=False):
+        self.name = name
+        self.leaf = leaf
+        self.vars = vars
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+
+def parent_graphs(child, groups):
+    results = _parents.get(child.name, list())
+    # Remove 'all' group from groups if a child has any other parent group
+    if len(groups) > 1:
+        allgroups = [g for g in groups if g.name == 'all']
+        if allgroups:
+            groups.remove(allgroups[0])
+    if not results:
+        for group in groups:
+            results.append(Edge(group.name, child.name))
+            if group.parent_groups:
+                results.extend(parent_graphs(group, group.parent_groups))
+        _parents[child.name] = results
+    return results
+
+def remove_inherited_and_overridden_vars(vars, group):
+    if group not in _vars:
+        _vars[group] = group.vars.copy()
+    gv = _vars[group]
+    for (k, v) in vars.items():
+        if k in gv: 
+            if gv[k] == v:
+                vars.pop(k)
+            else:
+                gv.pop(k)
+
+
+def remove_inherited_and_overridden_group_vars(group):
+    if group not in _vars:
+        _vars[group] = group.vars.copy()
+    for ancestor in group.get_ancestors():
+        remove_inherited_and_overridden_vars(_vars[group], ancestor)
+
+
+def tidy_all_the_variables(host):
+    ''' removes all overridden and inherited variables from hosts 
+        and groups '''
+    _vars[host] = host.vars.copy()
+    for group in host.get_groups():
+        remove_inherited_and_overridden_vars(_vars[host], group)
+    return _vars
+
+
+def generate_graph_for_host(host):
+    # dedup graph edges
+    edges = set(parent_graphs(host, host.groups))
+    vars = tidy_all_the_variables(host)
+    nodes = {Node(host.name, vars=vars[host], leaf=True)}
+
+    for group in host.get_groups():
+        nodes.add(Node(group.name, vars=vars[group]))
+
+    return (edges, nodes)
---
+++ test/TestVars.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import unittest
+
+import ansible.inventory
+import grapher
+
+
+class TestVars(unittest.TestCase):
+
+    def setUp(self):
+        invfile = os.path.join('test', 'inventory', 'hosts')
+        self.inventory = ansible.inventory.Inventory(invfile)
+        self.host = self.inventory.get_host("host")
+        self.vars = grapher.tidy_all_the_variables(self.host)
+
+    def test_gp_group_vars(self):
+        gp = self.inventory.get_group("grandparent")
+        vars = self.vars[gp]
+        self.assertEqual(vars.keys(), ["gp_not_overridden"])
+        self.assertEqual(vars["gp_not_overridden"], "gp")
+
+    def test_parent_group_vars(self):
+        parent = self.inventory.get_group("parent")
+        vars = self.vars[parent]
+        self.assertEqual(set(vars.keys()), set(["parent_not_overridden",
+                         "gp_overridden_in_parent"]))
+        self.assertEqual(vars["parent_not_overridden"], "parent")
+
+    def test_host_vars(self):
+        vars = self.vars[self.host]
+        self.assertEqual(vars["gp_overridden_in_child"], "child")
+        self.assertEqual(vars["parent_overridden_in_child"], "child")
+        self.assertEqual(vars["child_only"], "child")
+
