3f62c699a8ab23a0816dbaa00e13a7b3e3a081b6
Will Thames <will.thames@suncorp.com.au>
2014-09-18 18:40:12 +1000

    Ensure libraries get correctly found

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ from setuptools import setup
 sys.path.insert(0, os.path.abspath('lib'))
 
 setup(
-    name='ansibleinventorygrapher',
-    version='1.0.1',
+    name='ansible-inventory-grapher',
+    version='1.0.0',
     description='Creates graphs representing ansible inventory',
     author='Will Thames',
     author_email='will@thames.id.au',
     url='http://github.com/willthames/ansible-inventory-grapher',
     license='GPLv3',
     install_requires=['ansible >= 1.7'],
+    package_dir={'ansibleinventorygrapher': 'lib/ansibleinventorygrapher'},
     packages=['ansibleinventorygrapher'],
     scripts=['bin/ansible-inventory-grapher'],
 )
