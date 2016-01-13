4504f6725a67e0808875a5cc7623f2a611a6849b
Will Thames <will@thames.id.au>
2016-01-13 23:49:39 +1000

    Integrate with released Ansible 2.0
    * Fixes to meet new inventory API
    * Fix test cases so that setup is only called once
    * Improve host_vars rendering
    * Update version to 2.0.0 to match Ansible 2.0

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ sys.path.insert(0, os.path.abspath('lib'))
 
 setup(
     name='ansible-inventory-grapher',
-    version='1.0.3',
+    version='2.0.0',
     description='Creates graphs representing ansible inventory',
     author='Will Thames',
     author_email='will@thames.id.au',
     url='http://github.com/willthames/ansible-inventory-grapher',
     license='GPLv3',
-    install_requires=['ansible >= 1.7'],
+    install_requires=['ansible >= 2.0'],
     package_dir={'ansibleinventorygrapher': 'lib/ansibleinventorygrapher'},
     packages=['ansibleinventorygrapher'],
     scripts=['bin/ansible-inventory-grapher'],
--- test/TestVars.py
+++ test/TestVars.py
@@ -4,32 +4,37 @@ import unittest
 
 import ansible.inventory
 import ansibleinventorygrapher
-
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars import VariableManager
 
 class TestVars(unittest.TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
+        variable_manager = VariableManager()
+        loader = DataLoader()
         invfile = os.path.join('test', 'inventory', 'hosts')
-        self.inventory = ansible.inventory.Inventory(invfile)
-        self.host = self.inventory.get_host("host")
-        self.vars = grapher.tidy_all_the_variables(self.host)
+        cls.inventory = ansible.inventory.Inventory(loader=loader, variable_manager=variable_manager, host_list=invfile)
+        variable_manager.set_inventory(cls.inventory)
+        cls.host = cls.inventory.get_host("host")
+        cls.vars = ansibleinventorygrapher.tidy_all_the_variables(cls.host)
 
     def test_gp_group_vars(self):
         gp = self.inventory.get_group("grandparent")
-        vars = self.vars[gp]
-        self.assertEqual(vars.keys(), ["gp_not_overridden"])
-        self.assertEqual(vars["gp_not_overridden"], "gp")
+        gvars = self.vars[gp].copy()
+        self.assertEqual(gvars.keys(), ["gp_not_overridden"])
+        self.assertEqual(gvars["gp_not_overridden"], "gp")
 
     def test_parent_group_vars(self):
         parent = self.inventory.get_group("parent")
-        vars = self.vars[parent]
-        self.assertEqual(set(vars.keys()), set(["parent_not_overridden",
+        pvars = self.vars[parent].copy()
+        self.assertEqual(set(pvars.keys()), set(["parent_not_overridden",
                          "gp_overridden_in_parent"]))
-        self.assertEqual(vars["parent_not_overridden"], "parent")
+        self.assertEqual(pvars["parent_not_overridden"], "parent")
 
     def test_host_vars(self):
-        vars = self.vars[self.host]
-        self.assertEqual(vars["gp_overridden_in_child"], "child")
-        self.assertEqual(vars["parent_overridden_in_child"], "child")
-        self.assertEqual(vars["child_only"], "child")
+        hvars = self.vars[self.host].copy()
+        self.assertEqual(hvars["gp_overridden_in_child"], "child")
+        self.assertEqual(hvars["parent_overridden_in_child"], "child")
+        self.assertEqual(hvars["child_only"], "child")
 
