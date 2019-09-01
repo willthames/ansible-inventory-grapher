619fa676930f6545a340e78cac0eeaf850a52ce4
Will Thames <will@thames.id.au>
2019-09-01 22:18:26 +1000

    Update setup.py to use console entrypoint

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 sys.path.insert(0, os.path.abspath('lib'))
 
@@ -17,9 +17,15 @@ setup(
     author_email='will@thames.id.au',
     url='http://github.com/willthames/ansible-inventory-grapher',
     license='GPLv3',
-    install_requires=['ansible >= 1.9'],
-    package_dir={'ansibleinventorygrapher': 'lib/ansibleinventorygrapher'},
-    packages=['ansibleinventorygrapher'],
-    scripts=['bin/ansible-inventory-grapher'],
+    install_requires=['ansible >= 2.4'],
+    package_dir={'': 'lib'},
+    packages=find_packages('lib'),
+    include_package_data=True,
+    zip_safe=True,
+    entry_points={
+        'console_scripts': [
+             'ansible-inventory-grapher = ansibleinventorygrapher.__main__:main'
+        ]
+    },
     test_suite="test",
 )
