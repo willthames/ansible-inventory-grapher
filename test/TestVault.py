260f55aaf98e1661dbbbf98695b2843a4159bee6
Will Thames <will@thames.id.au>
2019-09-01 16:43:26 +1000

    Allow inline vault variables without vault password files
    We can compare inline vaulted values without needing to
    decrypt them. We just won't be able to show them very
    easily
    
    Fixes #40

--- test/TestVault.py
+++ test/TestVault.py
@@ -20,32 +20,24 @@ class TestVault(unittest.TestCase):
         invfile = os.path.join('test', 'vault', 'inventory')
         vault_password_files = [os.path.join('test', 'vault', 'vaultpass'),
                                 os.path.join('test', 'vault', 'notthevaultpass')]
-        try:
-            inventory_mgr = ansibleinventorygrapher.inventory.InventoryManager(invfile, False, vault_password_files)
-            hostname = "web-01"
-            host = inventory_mgr.inventory.get_host(hostname)
-            group = inventory_mgr.inventory.get_group("web")
-            the_vars = ansibleinventorygrapher.tidy_all_the_variables(host, inventory_mgr)
-            self.assertEqual(the_vars[group]["text"], "hello")
-        except NotImplementedError:
-            pass  # ansible-vault only supports multiple vault password files from 2.4
+        inventory_mgr = ansibleinventorygrapher.inventory.InventoryManager(invfile, False, vault_password_files)
+        hostname = "web-01"
+        host = inventory_mgr.inventory.get_host(hostname)
+        group = inventory_mgr.inventory.get_group("web")
+        the_vars = ansibleinventorygrapher.tidy_all_the_variables(host, inventory_mgr)
+        self.assertEqual(the_vars[group]["text"], "hello")
 
     def test_vault_ids(self):
         invfile = os.path.join('test', 'vault_ids', 'inventory')
         vault_ids = ['another_vault@' + os.path.join('test', 'vault_ids', 'vaultpass')]
-        try:
-            inventory_mgr = ansibleinventorygrapher.inventory.InventoryManager(invfile, False, [], vault_ids)
-            hostname = "web-01"
-            host = inventory_mgr.inventory.get_host(hostname)
-            the_vars = ansibleinventorygrapher.tidy_all_the_variables(host, inventory_mgr)
-            self.assertEqual(the_vars[host]["hello"], "world")
-        except NotImplementedError:
-            pass  # ansible-vault only supports vault-id from 2.4
+        inventory_mgr = ansibleinventorygrapher.inventory.InventoryManager(invfile, False, [], vault_ids)
+        hostname = "web-01"
+        host = inventory_mgr.inventory.get_host(hostname)
+        the_vars = ansibleinventorygrapher.tidy_all_the_variables(host, inventory_mgr)
+        self.assertEqual(the_vars[host]["hello"], "world")
 
     def test_no_vault_pass(self):
         invfile = os.path.join('test', 'vault', 'inventory')
-        vault_password_files = [os.path.join('test', 'vault', 'vaultpass'),
-                                os.path.join('test', 'vault', 'notthevaultpass')]
         try:
             inventory_mgr = ansibleinventorygrapher.inventory.InventoryManager(invfile, False, [])
             hostname = "web-01"
@@ -55,3 +47,12 @@ class TestVault(unittest.TestCase):
         except ansibleinventorygrapher.inventory.NoVaultSecretFound:
             pass  # expected behaviour
 
+    def test_inline_vault_without_password(self):
+        invfile = os.path.join('test', 'vault', 'inventory')
+        inventory_mgr = ansibleinventorygrapher.inventory.InventoryManager(invfile, False, [])
+        hostname = "inline-01"
+        host = inventory_mgr.inventory.get_host(hostname)
+        group = inventory_mgr.inventory.get_group("inline")
+        the_vars = ansibleinventorygrapher.tidy_all_the_variables(host, inventory_mgr)
+        self.assertTrue('text' in the_vars[group])
+        self.assertFalse('text' in the_vars[host])
